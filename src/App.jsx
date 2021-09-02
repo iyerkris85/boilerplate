67704fa72aa9e9b9799e26145a0147e66d429dd7
Krishnan Devarajan <iyerkris85@gmail.com>
2021-09-01 19:16:43 -0700

    - Fix App return

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import { ErrorBoundary } from 'react-error-boundary';
 import Main from './components/Main';
 import ErrorFallback from './components/ErrorFallback';
 
-function App() {
-  return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Main />
-    </ErrorBoundary>
-  );
-}
+const App = () => (
+  <ErrorBoundary FallbackComponent={ErrorFallback}>
+    <Main />
+  </ErrorBoundary>
+);
 
 export default App;
--- src/components/ErrorFallback/index.jsx
+++ src/components/ErrorFallback/index.jsx
@@ -9,19 +9,21 @@ import Alert from '@material-ui/lab/Alert';
 import PropTypes from 'prop-types';
 
 const ErrorFallback = ({ error, resetErrorBoundary }) => (
-  <Container fixed>
-    <div>
-      <Alert severity="error" m={2}>
-        Something went wrong:
-      </Alert>
-      <pre style={{ color: 'red' }} m={1}>
-        {error.message}
-      </pre>
-      <Button m={2} variant="contained" color="primary" onClick={resetErrorBoundary}>
-        Try again
-      </Button>
-    </div>
-  </Container>
+  <div>
+    <Container fixed>
+      <div>
+        <Alert severity="error" m={2}>
+          Something went wrong:
+        </Alert>
+        <pre style={{ color: 'red' }} m={1}>
+          {error.message}
+        </pre>
+        <Button m={2} variant="contained" color="primary" onClick={resetErrorBoundary}>
+          Try again
+        </Button>
+      </div>
+    </Container>
+  </div>
 );
 
 ErrorFallback.propTypes = {
